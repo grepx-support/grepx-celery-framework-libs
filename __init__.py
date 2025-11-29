4db07b55cfa4999828ad2be7bdd48d887466365b
Sridevi Sundararajan <support@grepx.sg>
2025-11-29 08:48:32 +0800

    refactor

--- __init__.py
+++ __init__.py
@@ -2,16 +2,13 @@
 """Generic Celery framework for distributed task execution."""
 
 __all__ = [
-    "CeleryApp",
     "CeleryConfig",
     "WorkerConfig",
     "TaskConfig",
     "TaskRegistry",
-    "CeleryFactory",
     "task",
 ]
 
 from src.config import CeleryConfig, WorkerConfig, TaskConfig
-from src.config.factory import CeleryFactory
 from src.tasks.decorators import task
 from src.tasks.task_registry import TaskRegistry
