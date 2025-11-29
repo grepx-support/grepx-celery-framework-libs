23a9abea92018c3da8f586676e8e4392f25a496e
Sridevi Sundararajan <support@grepx.sg>
2025-11-29 09:44:46 +0800

    refactor

--- src/services/__init__.py
+++
@@ -1,6 +0,0 @@
-# src/services/__init__.py
-"""Services module."""
-
-from .celery_service import celery_app, task_registry, task_loader, create_celery_service
-
-__all__ = ["celery_app", "task_registry", "task_loader", "create_celery_service"]
